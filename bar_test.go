a4fc262b2c58aadc4631d23b799b35a3b8fd9d06
ztind <1356798719@qq.com>
2020-07-30 00:46:29 +0800

    添加测试函数

--- bar_test.go
+++ bar_test.go
@@ -5,8 +5,17 @@ import (
 	"time"
 )
 
-func TestPrint(t *testing.T){
+func TestPrint1(t *testing.T){
 	bar := NewProgressBar(150)
+	for i:=0;i<=150;i++{
+		time.Sleep(time.Millisecond*100)
+		bar.Print(i)
+	}
+}
+
+
+func TestPrint2(t *testing.T){
+	bar := NewProgressBarFormat(150,">")
 	for i:=0;i<=150;i++{
 		time.Sleep(time.Millisecond*100)
 		bar.Print(i)
